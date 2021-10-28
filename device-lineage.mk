e1c32327883df03991b4afa4a3e144fa736297db
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    redbull: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-lineage.mk
+++ device-lineage.mk
@@ -17,7 +17,8 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
