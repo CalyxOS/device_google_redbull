d934b3f4f8087fcb013bae25997d22bfe3e51b5e
Tommy Webb <tommy@calyxinstitute.org>
2023-06-07 18:51:13 -0400

    Add WfcActivation permissions
    Change-Id: I899e8ff03e9c480deefa1307ae0e039caa0daa68

--- device-calyx.mk
+++ device-calyx.mk
@@ -8,7 +8,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
-    device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+    device/google/redbull/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
+    device/google/redbull/permissions/permissions_com.google.android.wfcactivation.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.wfcactivation.xml
 
 # HBM
 PRODUCT_COPY_FILES += \
