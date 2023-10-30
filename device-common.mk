bc0821e69543108050e5c8c8fc47dbe2ff46c3d3
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 02:36:59 +0200

    redbull: Cleanup dead targets
    Change-Id: I5f94d5f1cd00a4bf2907b0063bd997abad2b39df

--- device-common.mk
+++ device-common.mk
@@ -283,26 +283,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.audio.monitorRotation=true
 
-# Iaxxx streming and factory binary
-PRODUCT_PACKAGES += \
-    libtunnel \
-    libodsp \
-    adnc_strm.primary.default \
-    sound_trigger.primary.lito
-
-# Add Oslo test for debug rom
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-PRODUCT_PACKAGES += \
-    tunneling_hal_test \
-    sensor_param_test \
-    oslo_config_test \
-    odsp_api_test \
-    crash_event_logger \
-    dump_debug_info \
-    get_pwr_stats \
-    crash_trigger_test
-endif
-
 # graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610
@@ -394,21 +374,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
-# Bluetooth HAL
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl-qti \
-    android.hardware.bluetooth@1.0-service-qti
-
-#Bluetooth SAR HAL
-PRODUCT_PACKAGES += \
-    hardware.google.bluetooth.sar@1.0-impl
-PRODUCT_PACKAGES_DEBUG += \
-    bluetooth_sar_test
-
-#Bluetooth AFH HAL
-PRODUCT_PACKAGES += \
-    hardware.google.bluetooth.bt_channel_avoidance@1.0-impl
-
 # Bluetooth SoC
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=cherokee
@@ -427,8 +392,7 @@ PRODUCT_SOONG_NAMESPACES += vendor/qcom/proprietary/bluetooth/hidl_client
 
 # DRM HAL
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
-    android.hardware.drm-service.widevine-v17
+    android.hardware.drm-service.clearkey
 
 # NFC and Secure Element packages
 PRODUCT_PACKAGES += \
@@ -456,21 +420,8 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libstagefrighthw \
     libOmxVdec \
-    libOmxVdecHevc \
     libOmxVenc \
     libc2dcolorconvert
-
-# Enable Codec 2.0
-PRODUCT_PACKAGES += \
-    libqcodec2_base \
-    libqcodec2_utils \
-    libqcodec2_platform \
-    libqcodec2_core \
-    libqcodec2_basecodec \
-    libqcodec2_v4l2codec \
-    vendor.qti.media.c2@1.0-service \
-    codec2.vendor.ext-arm64.policy \
-    codec2.vendor.base-arm64.policy
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank=512
@@ -498,15 +449,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.mm_minqp=1
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.7-impl-google \
-    android.hardware.camera.provider@2.7-service-google \
-    camera.lito \
-    lib_multicam_dualfov_capture_session \
-    libgooglecamerahwl_impl \
-    libqomx_core \
-    libmmjpeg_interface \
-    libmmcamera_interface \
-    libcameradepthcalibrator
+    android.hardware.camera.provider@2.7-service-google
 
 # Google Camera HAL test libraries in debug builds
 PRODUCT_PACKAGES_DEBUG += \
@@ -545,7 +488,6 @@ endif
 PRODUCT_PACKAGES += $(HOSTAPD)
 
 WPA := wpa_supplicant.conf
-WPA += wpa_supplicant_wcn.conf
 WPA += wpa_supplicant
 PRODUCT_PACKAGES += $(WPA)
 
@@ -570,18 +512,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt \
     $(LOCAL_PATH)/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
 
-LIB_NL := libnl_2
-PRODUCT_PACKAGES += $(LIB_NL)
-
-# Audio effects
 PRODUCT_PACKAGES += \
-    libvolumelistener \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libqcompostprocbundle
-
-PRODUCT_PACKAGES += \
-    audio.primary.lito \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
@@ -593,17 +524,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.audio.service
-
-# Modules for Audio HAL
-PRODUCT_PACKAGES += \
-    libcirrusspkrprot \
-    libsndmonitor \
-    liba2dpoffload \
-    btaudio_offload_if \
-    libthermallistener \
-    libmaxxaudio \
-    libaudiozoom \
-    libdevicestatelistener
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
@@ -705,9 +625,6 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Override heap growth limit due to high display density on device
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit?=256m
@@ -715,10 +632,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Use 64-bit dex2oat for better dexopt time.
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
-
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
 
 #Set default CDMA subscription to RUIM
 PRODUCT_PROPERTY_OVERRIDES += \
