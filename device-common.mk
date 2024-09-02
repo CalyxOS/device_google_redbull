630446c27fa9a586de8ca5c8449937b79d150706
Michael Bestas <mkbestas@lineageos.org>
2024-09-02 18:09:03 +0300

    redbull: Move android.hardware.camera.provider@2.7-service-google to barbet
    b5r3 use prebuilt, which conflicts with source built
    when ELF checks are enabled
    
    Change-Id: If16b263f3df4924151c83a117b08600102aa985c

--- device-common.mk
+++ device-common.mk
@@ -23,7 +23,6 @@ PRODUCT_VENDOR_MOVE_ENABLED := true
 TARGET_BOARD_PLATFORM := lito
 
 PRODUCT_SOONG_NAMESPACES += \
-    hardware/google/camera \
     hardware/google/interfaces \
     hardware/google/pixel \
     device/google/redbull \
@@ -446,9 +445,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # To reach target bitrate in CBR mode for IMS VT Call
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.mm_minqp=1
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.7-service-google
 
 # Google Camera HAL test libraries in debug builds
 PRODUCT_PACKAGES_DEBUG += \
