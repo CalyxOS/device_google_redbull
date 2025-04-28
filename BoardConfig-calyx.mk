67d5930376769a99d71b5934e083d6033a6ac93e
Michael Bestas <mkbestas@lineageos.org>
2025-04-28 04:49:14 +0300

    redbull: Migrate gralloc_handle_has_reserved_size to soong_config_set
    Change-Id: I3aa081f081e41ba108ff91227409baad7f54d49c

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -22,6 +22,3 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 # SELinux
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/dynamic
 BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/vendor
-
-# Needed for camera
-TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
